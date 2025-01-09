98bf44b994a6021b97c63715166ecb667bd3d179
Laxmi Wandare <Laxmi.wandare@klingelenberg.com>
2025-01-09 16:22:24 +0530

    Added summary in Logger.cs

--- Logger.cs
+++ Logger.cs
@@ -3,16 +3,22 @@ namespace CustomerInformation
 {
     public static class Logger
     {
+        /// <summary>
+        /// Initialize Log to file logs/log.txt with RollingInterval of 1 day 
+        /// </summary>
         public static void LogInitializer()
         {
             Log.Logger = new LoggerConfiguration()
             .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
             .CreateLogger();
+            Log.Information("Log Initialized !");
         }
+        /// <summary>
+        /// closing log and Log.CloseAndFlush
+        /// </summary>
         public static void LogClose()
         {
             Log.Information("Program is Closing !");
-            System.Threading.Thread.Sleep(100);
             Log.CloseAndFlush();
         }
     }
